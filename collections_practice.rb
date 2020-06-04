c2af641a7efedb6dda36c3a7e61c47a95a1b6fd1
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-04 14:46:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(integers)
+  integers.sort 
+end
 
+def sort_array_desc(integers)
+  integers.sort
+end
+
+def sort_array_char_count(strings)
+  strings.sort 
+end
+
+def swap_elements(array)
+  
+end
+
+def reverse_array(integers)
+  
+end
+
+def kesha_maker(array)
+  new_array=[]
+  array.each do |name|
+  name[2]="$"
+  end 
+end
+
+def find_a(array)
+  
+end
+
+def sum_array(integers)
+  
+end
+
+def add_s(array)
+  
+end
